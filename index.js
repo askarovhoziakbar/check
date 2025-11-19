597e3ec2f37f1c7938e05c87dfdb1e3c9ab17023
askarovhoziakbar <askarovhoziakbar17@gmail.com>
2025-11-19 07:36:10 +0600

    сработай

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 import express from "express";
 import cors from "cors";
+import fs from "fs";
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
-let data = { click: 10 }; // объект хранится в памяти
+app.listen(3000);
 
-// GET /number
 app.get("/number", (req, res) => {
+  const data = { click: 0 };
   res.json(data);
 });
 
-// POST /number/increment
 app.post("/number/increment", (req, res) => {
   data.click += 1;
   res.json(data);
 });
-
-app.listen(3000, () => console.log("Server started on http://localhost:3000"));
