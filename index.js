9645311b2365a5467be83c9f11e6bc3d19b063ea
askarovhoziakbar <askarovhoziakbar17@gmail.com>
2025-11-19 07:10:11 +0600

    сработай

--- index.js
+++ index.js
@@ -9,10 +9,6 @@ app.use(express.json());
 
 const FILE = "click.json";
 
-if (!fs.existsSync(FILE)) {
-  fs.writeFileSync(FILE, JSON.stringify({ click: 0 }));
-}
-
 app.listen(3000);
 app.get("/number", (req, res) => {
   res.status(200).json("сервер работает");
