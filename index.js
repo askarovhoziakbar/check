9f0f4792e2f3ef533e95469ed629a5eb7f06b17b
askarovhoziakbar <askarovhoziakbar17@gmail.com>
2025-11-19 06:58:47 +0600

    сработай

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 import express from "express";
+import cors from "cors";
+import fs from "fs";
+
 const app = express();
-app.listen(3000);
-app.get("/", (req, res) => {
-  res.status(200).json("сервер работает");
+app.use(cors());
+app.use(express.json());
+const FILE = "click.json";
+
+if (!fs.existsSync(FILE)) {
+  fs.writeFileSync(FILE, JSON.stringify({ click: 0 }));
+}
+
+app.get("/number", (req, res) => {
+  const data = JSON.parse(fs.readFileSync(FILE, "utf-8"));
+  res.json(data);
+});
+
+app.post("/number/increment", (req, res) => {
+  const data = JSON.parse(fs.readFileSync(FILE, "utf-8"));
+  data.click += 1;
+
+  fs.writeFileSync(FILE, JSON.stringify(data));
+  res.json(data);
 });
